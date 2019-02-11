1967dd0ff3344b6d862745d074f75b714bb87c26
Pawel Mazur <ppmazur@gmail.com>
2019-02-11 20:51:03 +0100

    Removes css file and serviceWorker

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import logo from '../logo.svg';
+
 import GlobalStyle from '../theme/globalStyle';
 
 const AppWrapper = styled.div`
   text-align: left;
-`;
-
-const StyledHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
+  height: 100%;
 `;
 
 class App extends Component {
--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -1,9 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  html, body, #root {
+    height: 100%;
+  }
   body {
     padding: 0;
     margin: 0;
-    font-family: sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   }
 `;
