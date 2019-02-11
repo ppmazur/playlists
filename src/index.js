7fac150ee61b58b5ec9f191690d98812bfa31732
Pawel Mazur <ppmazur@gmail.com>
2019-02-11 21:13:33 +0100

    Adds support for redux-dev-tools

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import reducer from './reducers';
 
 import App from './components/App';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 ReactDOM.render(
   <Provider store={store}>
