e51f044ed136a68e77ce91b7af460989260a1ed0
Pawel Mazur <ppmazur@gmail.com>
2019-02-11 21:14:52 +0100

    Removes artists reducer
    Can be added in the future.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
-import artists from './artists';
 import playlists from './playlists';
 import songs from './songs';
 
 const playlistsApp = combineReducers({
-  artists,
   playlists,
   songs,
 });
--- src/reducers/songs.js
+++ src/reducers/songs.js
@@ -1,7 +1,7 @@
 const initState = {
   byId: {
-    S_1: { id: 'S_1', title: '111 Song', artistId: 'A_1' },
-    S_2: { id: 'S_2', title: '222 Song', artistId: 'A_1' },
+    S_1: { id: 'S_1', title: '111 Song', artist: 'Artist Name 1'},
+    S_2: { id: 'S_2', title: '222 Song', artist: 'Artist Name 2' },
   },
   allIds: ['S_1', 'S_2'],
 };
